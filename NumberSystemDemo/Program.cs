b399c3c3a0c9d762c871b3a6657e6c7258406e09
Sajith Dilhan <sajithdilhan@gmail.com>
2022-10-01 13:57:17 +0800

    Add project files.

---
+++ NumberSystemDemo/Program.cs
@@ -0,0 +1,187 @@
+﻿
+using System.Numerics;
+
+Console.WriteLine("Enter Number System: 1 - Binary, 2 - Octal, 3 - Hexa");
+string systemType = Console.ReadLine()!;
+Console.WriteLine("Enter Decimal Number");
+string decimalNumber = Console.ReadLine()!;
+
+if (string.IsNullOrEmpty(decimalNumber) || string.IsNullOrEmpty(systemType))
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Invalid Input!");
+    Console.ForegroundColor = ConsoleColor.White;
+    Thread.Sleep(300);
+    Environment.Exit(0);
+}
+
+try
+{
+    Stack<long> output = new Stack<long>();
+
+    if (systemType == "1")
+    {
+        output = ConvertToBinary(decimalNumber);
+        Console.Write("Binary number:");
+        foreach (var item in output)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(item);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    else if (systemType == "2")
+    {
+        output = ConvertToOctal(decimalNumber);
+        Console.Write("Octal number:");
+        foreach (var item in output)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(item);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    else if (systemType == "3")
+    {
+        Stack<string> hexaOutput = ConvertToHexa(decimalNumber);
+        Console.Write("Hexa number:");
+        foreach (var item in hexaOutput)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(item);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(ex.Message);
+    Console.ForegroundColor = ConsoleColor.White;
+    throw;
+}
+
+static Stack<long> ConvertToBinary(string decimalNumber)
+{
+    long decNumber = 0;
+    long.TryParse(decimalNumber, out decNumber);
+    Stack<long> output = new Stack<long>();
+
+    while (decNumber >= 2)
+    {
+        int remainder = ((int)(decNumber % 2));
+        output.Push(remainder);
+        decNumber = decNumber / 2;
+        if (decNumber == 1)
+        {
+            output.Push(decNumber);
+        }
+    }
+
+    return output;
+}
+
+static Stack<long> ConvertToOctal(string decimalNumber)
+{
+    long decNumber = 0;
+    long.TryParse(decimalNumber, out decNumber);
+    Stack<long> output = new Stack<long>();
+
+    while (decNumber >= 8)
+    {
+        int remainder = (int)(decNumber % 8);
+        output.Push(remainder);
+        decNumber = decNumber / 8;
+        if (decNumber < 8)
+        {
+            output.Push(decNumber);
+        }
+    }
+
+    return output;
+}
+
+static Stack<string> ConvertToHexa(string decimalNumber)
+{
+    BigInteger decNumber = 0;
+    BigInteger.TryParse(decimalNumber, out decNumber);
+    Stack<string> output = new Stack<string>();
+    string remainderStr = string.Empty;
+
+    while (decNumber >= 16)
+    {
+        int remainder = (int)(decNumber % 16);
+
+        if (remainder > 9)
+        {
+            switch (remainder)
+            {
+                case 10:
+                    remainderStr = "A";
+                    break;
+                case 11:
+                    remainderStr = "B";
+                    break;
+                case 12:
+                    remainderStr = "C";
+                    break;
+                case 13:
+                    remainderStr = "D";
+                    break;
+                case 14:
+                    remainderStr = "E";
+                    break;
+                case 15:
+                    remainderStr = "F";
+                    break;
+                default:
+                    break;
+            }
+            output.Push(remainderStr);
+        }
+        else
+        {
+            output.Push(remainder.ToString());
+        }
+
+        decNumber = decNumber / 16;
+        int integer;
+        if (decNumber < 16)
+        {
+            integer = int.Parse(decNumber.ToString());
+            if (integer > 9)
+            {
+                switch (integer)
+                {
+                    case 10:
+                        remainderStr = "A";
+                        break;
+                    case 11:
+                        remainderStr = "B";
+                        break;
+                    case 12:
+                        remainderStr = "C";
+                        break;
+                    case 13:
+                        remainderStr = "D";
+                        break;
+                    case 14:
+                        remainderStr = "E";
+                        break;
+                    case 15:
+                        remainderStr = "F";
+                        break;
+                    default:
+                        break;
+                }
+                output.Push(remainderStr);
+            }
+            else
+            {
+                output.Push(decNumber.ToString());
+            }
+        }
+    }
+
+    return output;
+}
